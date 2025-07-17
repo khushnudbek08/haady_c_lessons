bddde519161eccbafacc998c0ad1815f33724806
Harle Oberyn <harleobe@sa-i2.tash.21-school.uz>
2025-07-17 16:25:44 +0500

    3-dars tugatildi

---
+++ lesson_3_variables/les3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+        char sinf = 'A';
+        int age = 22;
+        float heg = 1.78;
+        char name[7] = "Xushnud"; // aval ko'rsatiladigon hajim string sonidan 1 taga ko'p bo'lishi kerak
+
+        printf("Salom mening ismin %s, yoshim %d, bo'yim esa %f, yoshligimda %c sinf da o'qiganman\n", name, age, heg, sinf);
+        return 0;
+
+}
+
