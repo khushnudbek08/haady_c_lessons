12489c1aca3e75c71270aaa4e11ad0e4a7ccd61f
khushnud <khushnudkozokjonov@gmail.com>
2025-08-02 21:37:43 +0500

    done

--- lesson_8_while/less8.c
+++ lesson_8_while/less8.c
@@ -2,3 +2,26 @@
 Masala: Foydalanuvchi son kiritadi.
 Har gal: "Juft" yoki "Toq" deb chiqaring.
 0 kiritsagina dasturni tugating.*/
+
+#include <stdio.h>
+
+int main() {
+  int input;
+
+  while (1) {
+    printf("\nraqam kiriting: ");
+    scanf("%d", &input);
+    if (input == 0) {
+      printf("You finished the game!\n");
+      break;
+    } else {
+      if (input % 2 == 0) {
+        printf("Juft son\n");
+      } else {
+        printf("toq son\n");
+      }
+    }
+  }
+
+  return 0;
+}
--- lesson_8_while/less9.c
+++ lesson_8_while/less9.c
@@ -1,7 +1,29 @@
 /*
 ✅ 9. goto bilan tozalash
-Masala: Har qanday 3 ta sonni kiriting. 
-Agar sonlardan biri manfiy bo‘lsa, goto orqali qaytadan boshlang. 
+Masala: Har qanday 3 ta sonni kiriting.
+Agar sonlardan biri manfiy bo‘lsa, goto orqali qaytadan boshlang.
 Hammasi musbat bo‘lsa, ularning o‘rtacha qiymatini hisoblang.
 
 */
+
+#include <stdio.h>
+
+int main() {
+  int a, b, c;
+boshi:
+  printf("uchta son kriting: ");
+  if (scanf("%d %d %d", &a, &b, &c) != 3) {
+    printf("n/a");
+    return 1;
+  } else {
+    if ((a < 0) || (b < 0) || (c < 0)) {
+      printf("iltimos musbat son kriting!\n");
+      goto boshi;
+    } else {
+      float avg = (a + b + c) / 3;
+      printf("ortacha qiymat: %.2f\n", avg);
+    }
+  }
+
+  return 0;
+}
